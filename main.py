f3f8dffb4a20807a64f2834b5aa3c40cdbcb9a85
Your Name <glen53simos53@gmail.com>
2021-05-24 16:10:02 -0600

    We got her

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 
 
 def array_merge_sort(n, m, array): # n rows, m columns
-    print(array, n, m)
 
     if n == 1:
         if m == 1:
             return array
         #print('ok', array)
         temp = one_d_merge_sort(m, array[0])
-        print(temp, 10000)
-        output = [temp[i] for i in range(len(temp))]
-        return output
+        #print(temp, 10000)
+
+        return [temp]
 
     elif m == 1:
-        temp = one_d_merge_sort(n, array)
+
+        temp = [[array[i][0] for i in range(n)]]
+        temp = one_d_merge_sort(n, temp[0])
+
         #a = [temp[i] for i in range(m)]
         #print(temp)
-        return [temp[i] for i in range(m)]
+        #print(temp, 200)
+        return [[temp[i]] for i in range(n)]
 
 
     rowsplit = 1 + int((n - 1) / 2)
@@ -24,16 +27,17 @@ def array_merge_sort(n, m, array): # n rows, m columns
     #print(rowsplit, colsplit)
 
 
-    topleft = [array[i][0:rowsplit] for i in range(0, colsplit)]
-    topright = [array[i][rowsplit:n] for i in range(0, colsplit)]
-    bottomleft = [array[i][0:rowsplit] for i in range(colsplit, m)]
-    bottomright = [array[i][rowsplit:n] for i in range(colsplit, m)]
-    print(topleft, topright, bottomleft, bottomright)
+    topleft = [array[i][0:colsplit] for i in range(0, rowsplit)]
+    topright = [array[i][colsplit:m] for i in range(0, rowsplit)]
+    bottomleft = [array[i][0:colsplit] for i in range(rowsplit, n)]
+    bottomright = [array[i][colsplit:m] for i in range(rowsplit, n)]
+    #print(rowsplit, colsplit, topleft, topright, bottomleft, bottomright)
 
     topleft = array_merge_sort(rowsplit, colsplit, topleft)
-    topright = array_merge_sort(n - rowsplit, colsplit, topright)
-    bottomleft = array_merge_sort(rowsplit, m - colsplit, bottomleft)
+    topright = array_merge_sort(rowsplit, m - colsplit, topright)
+    bottomleft = array_merge_sort(n - rowsplit, colsplit, bottomleft)
     bottomright = array_merge_sort(n - rowsplit, m - colsplit, bottomright)
+
 
     #print(topleft, topright, bottomleft, bottomright)
     return merge(n, m, rowsplit, colsplit, topleft, topright, bottomleft, bottomright)
@@ -56,13 +60,15 @@ def one_d_merge_sort(n, arr):
         #print(L, R)
         #print(arr, mid, L, R)
         # Sorting the first half
-        L = one_d_merge_sort(mid,L)
+        L = one_d_merge_sort(mid, L)
         # Sorting the second half
         R = one_d_merge_sort(n-mid, R)
         i = j = k = 0
         #print(R, L)
         # Copy data to temp arrays L[] and R[]
-        return one_d_merge(R, L)
+        out = one_d_merge(R, L)
+
+        return out
     else:
         return arr
 
@@ -72,22 +78,18 @@ def one_d_merge_sort(n, arr):
 
 def merge(rows, cols, rowsplit, colsplit, a, b, c, d): # first merges rows together, then iterates through columns
     #row sorting
-    toprows = [one_d_merge(a[i], b[i]) for i in range(0, colsplit)]
+    toprows = [one_d_merge(a[i], b[i]) for i in range(0, rowsplit)]
     bottomrows = [one_d_merge(c[i], d[i]) for i in range(0, cols - colsplit)]
     rowarray = toprows + bottomrows
     #column sorting
     #print(rowarray)
-    for i in range(0, rowsplit): # a and c
+    for i in range(0, cols): # a and c
 
-        ac_column = one_d_merge([rowarray[j][i] for j in range(colsplit)], [rowarray[j][i] for j in range(colsplit, cols)])
+        ac_column = one_d_merge([rowarray[j][i] for j in range(rowsplit)], [rowarray[j][i] for j in range(rowsplit, rows)])
         #print(ac_column, i)
         for index, element in enumerate(ac_column):
             rowarray[index][i] = element
-    for i in range(rowsplit, rows): # b and d
-        bd_column = one_d_merge([rowarray[j][i] for j in range(colsplit)], [rowarray[j][i] for j in range(colsplit, cols)])
-        #print(bd_column)
-        for index, element in enumerate(bd_column):
-            rowarray[index][i] = element
+
 
     #print(rowarray)
     return rowarray
@@ -119,4 +121,25 @@ def one_d_merge(list1, list2): # assumes each sub list are sorted, then combines
     return output
 
 if __name__ == '__main__':
-    print(array_merge_sort(4, 4, [[18, 4, 16, 8], [23, 13, 20 ,11], [28, 24, 26, 25], [1 ,30, 15, 19]]))
+    '''a = []
+    #print(a)
+    n = int(input("Num Rows: "))
+    m = int(input("Num Cols: "))
+    for i in range(n):
+        a.append([])
+        for j in range(m):
+            a[i].append(int(input("array value: ")))
+
+    print(a)
+    print(array_merge_sort(n, m, a))'''
+
+    n, m = map(int, input("N, M: ").split())
+
+    temparray = input("Enter Values: ").split()
+    array = []
+    for i in range(n):
+        array.append([])
+        for j in range(m):
+            index = i * n + j
+            array[i].append(int(temparray[index]))
+    print(array_merge_sort(n, m, array))
