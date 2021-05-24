c5c46dae1b3e4754c53dea55acf6432b5d89af9e
Your Name <glen53simos53@gmail.com>
2021-05-24 16:21:25 -0600

    Commented now, should work well

--- main.py
+++ main.py
@@ -1,54 +1,51 @@
 
 
 def array_merge_sort(n, m, array): # n rows, m columns
-
+    #end conditions, if n or m == 1, returns a sorted version with the correct dimensions
     if n == 1:
         if m == 1:
             return array
-        #print('ok', array)
         temp = one_d_merge_sort(m, array[0])
-        #print(temp, 10000)
-
         return [temp]
 
     elif m == 1:
 
-        temp = [[array[i][0] for i in range(n)]]
-        temp = one_d_merge_sort(n, temp[0])
+        temp = [array[i][0] for i in range(n)]
+        temp = one_d_merge_sort(n, temp)
 
         #a = [temp[i] for i in range(m)]
         #print(temp)
         #print(temp, 200)
         return [[temp[i]] for i in range(n)]
 
-
+    # finds the correct splitting point for rows and columns
     rowsplit = 1 + int((n - 1) / 2)
     colsplit = 1 + int((m - 1) / 2)
-    #print(rowsplit, colsplit)
 
 
+    # finds the 4 quadrants of the array
     topleft = [array[i][0:colsplit] for i in range(0, rowsplit)]
     topright = [array[i][colsplit:m] for i in range(0, rowsplit)]
     bottomleft = [array[i][0:colsplit] for i in range(rowsplit, n)]
     bottomright = [array[i][colsplit:m] for i in range(rowsplit, n)]
     #print(rowsplit, colsplit, topleft, topright, bottomleft, bottomright)
 
+    # recursively sorts each quadrant
     topleft = array_merge_sort(rowsplit, colsplit, topleft)
     topright = array_merge_sort(rowsplit, m - colsplit, topright)
     bottomleft = array_merge_sort(n - rowsplit, colsplit, bottomleft)
     bottomright = array_merge_sort(n - rowsplit, m - colsplit, bottomright)
 
 
-    #print(topleft, topright, bottomleft, bottomright)
+    # merges the 4 sorted quadrants, creating one big sorted array
     return merge(n, m, rowsplit, colsplit, topleft, topright, bottomleft, bottomright)
 
 
 
 
 
-def one_d_merge_sort(n, arr):
+def one_d_merge_sort(n, arr): # literally just a merge sort stolen from the internet, same idea as 2d, just simplified
     if n > 1:
-
         # Finding the mid of the array
         mid = 1 + int((len(arr) - 1) / 2)
         #print(arr, mid)
@@ -74,7 +71,6 @@ def one_d_merge_sort(n, arr):
 
 
 
-    # Code to print the
 
 def merge(rows, cols, rowsplit, colsplit, a, b, c, d): # first merges rows together, then iterates through columns
     #row sorting
@@ -82,14 +78,11 @@ def merge(rows, cols, rowsplit, colsplit, a, b, c, d): # first merges rows toget
     bottomrows = [one_d_merge(c[i], d[i]) for i in range(0, cols - colsplit)]
     rowarray = toprows + bottomrows
     #column sorting
-    #print(rowarray)
-    for i in range(0, cols): # a and c
-
+    for i in range(0, cols):
         ac_column = one_d_merge([rowarray[j][i] for j in range(rowsplit)], [rowarray[j][i] for j in range(rowsplit, rows)])
         #print(ac_column, i)
-        for index, element in enumerate(ac_column):
+        for index, element in enumerate(ac_column): # changes values in rowarray to match the column sorts
             rowarray[index][i] = element
-
 
     #print(rowarray)
     return rowarray
@@ -98,7 +91,7 @@ def one_d_merge(list1, list2): # assumes each sub list are sorted, then combines
     output = [0] * (len(list1) + len(list2))
 
     i, j, k = 0, 0, 0
-    while i < len(list1) and j < len(list2):
+    while i < len(list1) and j < len(list2): # this while adds the lesser of the two elements to the output over and over
         if list1[i] < list2[j]:
             output[k] = list1[i]
             i += 1
@@ -117,29 +110,18 @@ def one_d_merge(list1, list2): # assumes each sub list are sorted, then combines
         output[k] = list2[j]
         j += 1
         k += 1
-    #print(output, 100)
+
     return output
 
 if __name__ == '__main__':
-    '''a = []
-    #print(a)
-    n = int(input("Num Rows: "))
-    m = int(input("Num Cols: "))
-    for i in range(n):
-        a.append([])
-        for j in range(m):
-            a[i].append(int(input("array value: ")))
 
-    print(a)
-    print(array_merge_sort(n, m, a))'''
+    n, m = map(int, input("N, M: ").split()) # gets n and m values
 
-    n, m = map(int, input("N, M: ").split())
-
-    temparray = input("Enter Values: ").split()
+    temparray = input("Enter Values: ").split() # gets a list of each number as a string from inputs, has size n*m
     array = []
-    for i in range(n):
+    for i in range(n): # this loop maps the raw temparray to a correctly sized array
         array.append([])
         for j in range(m):
             index = i * n + j
             array[i].append(int(temparray[index]))
-    print(array_merge_sort(n, m, array))
+    print(array_merge_sort(n, m, array)) # prints a n * m 2d sorted list
